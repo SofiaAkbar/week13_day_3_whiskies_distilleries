4099279d3e47132195c0e5df497153135d9e4023
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-10-13 15:34:10 +0100

    extensions done

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -11,4 +11,6 @@ public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
     List<Whisky> findByYear(Integer year);
 
     List<Whisky> findByDistilleryNameAndAge(String distillery, Integer age);
+
+    List<Whisky> findByDistilleryRegion(String region);
 }
