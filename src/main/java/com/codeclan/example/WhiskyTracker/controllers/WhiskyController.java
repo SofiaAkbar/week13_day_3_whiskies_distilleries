4b405c57d16611ff38db235b777fb881d449be51
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-10-13 15:18:08 +0100

    mvp done

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,41 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> findWhiskyByYear (
+            @RequestParam(name= "year", required = false) Integer year,
+            @RequestParam(name= "distillery", required = false) String distillery,
+            @RequestParam(name= "age", required = false) Integer age){
+        if (year != null){
+            return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
+        } else if (distillery != null && age != null) {
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(distillery, age), HttpStatus.OK);
+        }
+            return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+
+
+}
